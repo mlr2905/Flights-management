75f1345fa9ac4bd67c4c1fc12c63264e8eec43e9
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-12-31 14:28:51 +0200

    up

---
+++ insert_flights.sql
@@ -0,0 +1,5 @@
+INSERT INTO flights (airline_id,origin_country_id,destination_country_id,departuer_time,landing_time,remaining_tickets)
+VALUES
+  (3,74,4,'2024-1-21 05:00','2024-1-21 07:00',200 ),
+  (10,74,21,'2024-1-30 05:00','2024-1-30 16:00',50 ),
+  (16,74,19,'2024-2-14 07:00','2024-2-14 18:00',50 );
