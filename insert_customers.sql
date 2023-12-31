8648ef19dca56cb721d7d33211f42d04a47e03fc
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-12-31 16:11:46 +0200

    up

--- insert_customers.sql
+++ insert_customers.sql
@@ -1,6 +1,5 @@
-insert into customers(first_name, last_name, address,phone_no,credit_card_no,user_id)
-    VALUES ('idit', 'rozental', 'israel','+972503424253','2255','1');
-insert into customers(first_name, last_name, address,phone_no,credit_card_no,user_id)
-    VALUES ('meital', 'rozental', 'israel','+972507462964','4422','2');
-insert into customers(first_name, last_name, address,phone_no,credit_card_no,user_id)
-    VALUES ('yosef', 'rozental', 'israel','+972506372833','6586','3');
+INSERT INTO customers (first_name, last_name, address, phone_no, credit_card_no, user_id)
+VALUES
+('idit', 'rozental', 'israel', '+972503424253', '2255', '1'),
+('meital', 'rozental', 'israel', '+972507462964', '4422', '2'),
+('yosef', 'rozental', 'israel', '+972506372833', '6586', '3');
--- insert_users.sql
+++ insert_users.sql
@@ -1,6 +1,5 @@
-insert into users(username, password, email)
-    VALUES ('idit', '1234', 'idit@gmail.com');
-insert into users(username, password, email)
-    VALUES ('meital', '1234', 'meital@gmail.com');
-insert into users(username, password, email)
-    VALUES ('yosef', '1234', 'yosef@gmail.com');
+INSERT INTO users (username, password, email)
+VALUES
+('idit', '1234', 'idit@gmail.com'),
+('meital', '1234', 'meital@gmail.com'),
+('yosef', '1234', 'yosef@gmail.com');
