52b787d81914db6f65fd8d01b471427632b4671d
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-11 00:27:28 +0200

    up

--- create_tickets.sql
+++ create_tickets.sql
@@ -1,10 +1,10 @@
 create table tickets (
 	id  SERIAL  PRIMARY KEY,
-	flight_id  BIGINT   NOT NULL,
+	flight_id  INT   NOT NULL,
 	FOREIGN KEY (flight_id) REFERENCES flights(id),
-	customer_id  BIGINT  UNIQUE  NOT NULL,
+	customer_id  INT  UNIQUE  NOT NULL,
 	FOREIGN KEY (customer_id) REFERENCES customers(id)
-	passenger_id  BIGINT  UNIQUE  NOT NULL,
+	passenger_id  INT  UNIQUE  NOT NULL,
 		FOREIGN KEY (passenger_id) REFERENCES passengers(id)
 
 	)
